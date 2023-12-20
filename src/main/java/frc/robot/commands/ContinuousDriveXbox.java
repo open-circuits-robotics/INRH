6b740566ef83447ad4397e91f445a50b41783c76
Halie <haliekarre@gmail.com>
2023-12-20 15:33:07 -0600

    Removed unused imports

--- src/main/java/frc/robot/commands/ContinuousDriveXbox.java
+++ src/main/java/frc/robot/commands/ContinuousDriveXbox.java
@@ -1,8 +1,6 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.DriveMath;
 import frc.robot.DriveState;
 import frc.robot.subsystems.ControlSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
--- src/main/java/frc/robot/commands/DiagnosticCommand.java
+++ src/main/java/frc/robot/commands/DiagnosticCommand.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Motors;
-import frc.robot.subsystems.ControlSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
 public class DiagnosticCommand extends CommandBase {
