8bb17116caa99dffb863baeb6a026fd5ce10eacb
open-circuits-robotics <owlrobotics5271@gmail.com>
2024-02-14 15:30:35 -0600

    made it not be backwards

--- src/main/java/frc/robot/hardware/Motors.java
+++ src/main/java/frc/robot/hardware/Motors.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Motors {
     //motors for new robot
-    public final static PWMSparkMax leftDrive = new PWMSparkMax(2);
-    public final static PWMSparkMax leftDriveTwo = new PWMSparkMax(3);
-    public final static PWMSparkMax rightDrive = new PWMSparkMax(0);
-    public final static PWMSparkMax rightDriveTwo = new PWMSparkMax(1);
+    public final static PWMSparkMax leftDrive = new PWMSparkMax(1);
+    public final static PWMSparkMax leftDriveTwo = new PWMSparkMax(0);
+    public final static PWMSparkMax rightDrive = new PWMSparkMax(3);
+    public final static PWMSparkMax rightDriveTwo = new PWMSparkMax(2);
 
     public final static PWMSparkMax leftIntakeMotor = new PWMSparkMax(4);
     public final static PWMSparkMax rightIntakeMotor = new PWMSparkMax(5);
