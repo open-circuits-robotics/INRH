e502f4c6587801e81b49aef4cfd9974a7dc4ac1d
5271 <opencircuitsrobotics@gmail.com>
2024-02-17 10:55:32 -0600

    fixed sen

--- src/main/java/frc/robot/DriveState.java
+++ src/main/java/frc/robot/DriveState.java
@@ -25,6 +25,6 @@ public class DriveState {
         driveTrainSubsystem.differentialDrive.arcadeDrive(xSpeed, rotation);
         System.out.println(xSpeed + " " + rotation);
         intakeSubsystem.intakeMotors.set(intake*0.5);
-        intakeSubsystem.shootingMotors.set(shooter);
+        intakeSubsystem.shootingMotors.set(intake);
     }
 }
