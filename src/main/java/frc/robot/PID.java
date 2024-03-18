4ed0626863d784319ea0309a83ecd77bec6a6fb5
Jocelyn <jcompton002@stpaul.k12.mn.us>
2024-03-18 15:26:57 -0500

    wow pid guys

--- src/main/java/frc/robot/PID.java
+++ src/main/java/frc/robot/PID.java
@@ -1,5 +1,24 @@
 package frc.robot;
 
 public class PID {
-    
+    public double getPIDOutput(double setpoint, double position, double time, double lastSetpoint, double lastPosition, double lastTime, double integral, double integralClamp, double kP, double kI, double kD) {
+		double error = setpoint - position;
+		double lastError = lastSetpoint - lastPosition;
+		double outputP = kP * error;
+		integral = clamp(-integralClamp, integralClamp, integral + (error * (time - lastTime)));
+		double outputI = kI * integral;
+		double outputD = kD * ((error - lastError)/time - lastTime);
+		double output = clamp(-1, 1, outputP + outputI + outputD);
+		return output;
+	}
+
+    public double clamp(double min, double max, double value){
+        if (value < min){
+            return min;
+        } else if (value > max){
+            return max;
+        } else {
+            return value;
+        }
+    }
 }
